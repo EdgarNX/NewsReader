196b9b5de75d5507ff4b6f9afcc731c510dfba7c
EdgarNX <edgarnemeth@gmail.com>
2021-06-02 21:33:24 +0300

    Updates on UI for a list of news.

--- app/src/main/java/com/nemethedgar/newsreader/model/ArticleItemViewModel.java
+++ app/src/main/java/com/nemethedgar/newsreader/model/ArticleItemViewModel.java
@@ -10,13 +10,13 @@ public class ArticleItemViewModel {
 
     @Nullable
     public Integer id;
-    public final ObservableField<String> newsTitle;
-    public final ObservableField<String> newsContent;
+    public final String newsTitle;
+    public final String newsContent;
     public final String articleImageURL;
 
-    public ArticleItemViewModel() {
-        this.newsTitle = new ObservableField<>();
-        this.newsContent = new ObservableField<>();
-        this.articleImageURL = "https://cdn.pixabay.com/photo/2015/03/26/09/47/sky-690293__340.jpg";
+    public ArticleItemViewModel(String newsTitle, String newsContent, String articleImageURL) {
+        this.newsTitle = newsTitle;
+        this.newsContent = newsContent;
+        this.articleImageURL = articleImageURL;
     }
 }
--- app/src/main/java/com/nemethedgar/newsreader/model/NewsListViewModel.java
+++ app/src/main/java/com/nemethedgar/newsreader/model/NewsListViewModel.java
@@ -8,6 +8,8 @@ import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
 
+import static android.provider.Settings.System.getString;
+
 public class NewsListViewModel extends ViewModel implements LifecycleObserver {
 
     @NonNull
@@ -16,16 +18,11 @@ public class NewsListViewModel extends ViewModel implements LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     public void refresh() {
         if (newsList.isEmpty()) {
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
-            newsList.add(new ArticleItemViewModel());
+            newsList.add(new ArticleItemViewModel("Unu", "contect content content content content content", "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg"));
+            newsList.add(new ArticleItemViewModel("Trei", "contect content content content content content", "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg"));
+            newsList.add(new ArticleItemViewModel("Patru", "contect content content content content content", "https://images.unsplash.com/photo-1590767187868-b8e9ece0974b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"));
+            newsList.add(new ArticleItemViewModel("Cinci", "contect content content content content content", "https://webmeup.com/upload/blog/lead-image-105.png"));
+            newsList.add(new ArticleItemViewModel("Sase", "contect content content content content content", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy1uZTBlxjOfVEiZsIt9FSo_bkxgEb6_OslQ&usqp=CAU"));
         }
     }
 
